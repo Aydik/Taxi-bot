83fc8ec383e18280b7d3bacc93f9c4f9f5c6d90b
Aydik <nekkisuper@gmail.com>
2022-06-22 15:28:37 +0400

    complete

--- app.js
+++ app.js
@@ -1,8 +1,69 @@
-import { Sequelize, Model, DataTypes } from 'sequelize';
+import { Sequelize, Model, DataTypes, where } from 'sequelize'
+import express from 'express'
+import { engine } from 'express-handlebars'
+import { readFile } from 'fs.promises'
+
 
 const sequelize = new Sequelize('postgres://taxi_bot_user:789456123@localhost:5432/taxi_bot')
 
-const User = sequelize.define('config', {
+
+const app = express()
+app.use(express.urlencoded({ extended: true }))
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+app.use(express.static(`${__dirname}/assets`));
+
+const types = {
+  host: function(value) {
+    var arr = value.split(".")
+    if (arr.length != 4){
+      return false
+    }
+    for(var item in arr){
+      if(isNaN(arr[item])){
+        return false
+      }
+      if (255 < arr[item] || arr[item] < 0){
+        return false
+      }
+    }
+    return true
+  },
+  port: function(value) {
+    if (isNaN(value)){
+      return false
+    }
+    if (65535 < value || value < 1024){
+      return false
+    }
+    return true
+  },
+  url: function validURL(value) {
+    var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+      '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+      '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+      '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+    return !!pattern.test(value);
+  }
+}
+
+async function loadMapping () {
+  try{
+    var render = JSON.parse(await readFile("mapping.json", "utf8"));
+    return render
+  }
+  catch(e){
+    console.error(e)
+  }
+  return null
+}
+
+const Config = sequelize.define('config', {
   name: {
     type: DataTypes.TEXT,
     allowNull: false,
@@ -17,17 +78,107 @@ const User = sequelize.define('config', {
 }
 );
 
-// const jane = User.create({
-//   username: 'janedoe',
-//   birthday: new Date(1980, 6, 20),
-// });
-async function  init () {
-  try {
-    const users = await User.findAll();
-    console.log('Connection has been established successfully.', users);
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  } 
+const Log = sequelize.define('log', {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    unique: true,
+    primaryKey: true
+  },
+  text: DataTypes.TEXT,
+  time: DataTypes.TEXT
+},
+{
+  tableName: 'log',
+  timestamps: false
 }
+);
 
-init()
+app.get('/', async function (_, response) {
+  const config = await Config.findAll();
+  const data = config.map(({dataValues}) => dataValues)
+  console.log(data)
+
+  response.render('home', { data } )
+})
+
+app.get('/users', async function (_, response) {
+  const users = await Log.findAll();
+  const usernames = users.map(({dataValues}) => {
+    const payload = JSON.parse(dataValues.text)
+    return payload.username
+  })
+  const tablenames = {}
+  for (var user in usernames){
+    var name = usernames[user]
+    if (name in tablenames){
+      tablenames[name] += 1
+    }
+    else{
+      tablenames[name] = 1
+    }
+  }
+  console.log(tablenames)
+  response.render('users', {tablenames})
+})
+
+app.get('/log', async function (request, response) {
+  var page = parseInt(request.query.page)
+  var nextpage = page + 1
+  var prevpage = page - 1
+  const log = await Log.findAll({
+      limit: 100,
+      offset: (page - 1) * 100
+    });
+  const data = log.map(({dataValues}) => {
+    const payload = JSON.parse(dataValues.text)
+    return {
+      id: dataValues.id,
+      username: payload.username,
+      name: payload.name,
+      text: payload.text,
+      time: dataValues.time
+    }
+  })
+  console.log(data)
+  if (data.length < 100){
+    nextpage = null
+  }
+  response.render('log', { data, page, nextpage, prevpage})
+})
+
+app.post('/save', async function (request, response){
+  const newData = request.body
+  console.log(newData)  
+  
+const mapping = await loadMapping()
+
+  try{
+    for (const key in newData){
+      var flag = true
+      if (key in mapping){
+        const validate = types[mapping[key].type]
+        flag = validate(newData[key])
+      }
+      if (flag){
+        await Config.update({data: newData[key]}, {
+          where: {
+            name: key
+          }
+        })
+      }
+      else{
+        response.send("Значение в поле " + key + " некорректное")
+        return 
+      }
+      }
+
+    response.send("Data updated")
+  }
+  catch(e){
+    console.error(e)
+    response.send(e)
+  }
+})
+
+app.listen(4000,()=> console.log('app started'))
